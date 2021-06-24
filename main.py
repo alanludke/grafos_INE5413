b48349969dc44af371e10871821a39d07840fb97
Alan Lüdke <alanludke@gmail.com>
2021-06-24 10:51:17 -0300

    declaring the methods and including the job desc

---
+++ main.py
@@ -0,0 +1,26 @@
+def qtdVertices():
+    pass
+
+def qtdArestas():
+    pass
+
+def grau(v):
+    pass
+
+def rotulo(v):
+    pass
+
+def vizinhos(v):
+    pass
+
+def haAresta(v):
+    pass
+
+def peso(u,v):
+    pass
+
+def main():
+    print("printing main")
+
+if __name__ == '__main__':
+    main()
