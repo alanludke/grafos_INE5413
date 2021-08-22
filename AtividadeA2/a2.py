0cc1db8ad59bfd3aab96baed7a21f000be3f02d3
rafaelbcastilhos <rafaelbcastilhos@gmail.com>
2021-08-22 19:32:40 -0300

    fix a2 and reformat pep8

--- AtividadeA2/a2.py
+++ AtividadeA2/a2.py
@@ -16,9 +16,9 @@ class Grafo:
             self.__qtd_vertices = qtdVertices
             for _ in range(qtdVertices):
                 linha = arquivo.readline()
-                self.__rotulos.append(linha[linha.index(" ")+1:-1])
+                self.__rotulos.append(linha[linha.index(" ") + 1:-1])
                 temp = linha.split(' "')
-                k,v = temp[0],temp[1]
+                k, v = temp[0], temp[1]
                 self.__vertices[int(k)] = v.rstrip("\n").strip('\"')
 
             self.__vetor = [[] for _ in range(qtdVertices)]
@@ -117,6 +117,7 @@ class Grafo:
         return transposto
 
 
+# --------------------- Q1 -----------------------------
 def visita(grafo, u, C, T, A, F, tempo):
     C[u - 1] = True  # visitado como verdadeiro
     tempo += 1
@@ -171,30 +172,34 @@ def componentes_fortemente_conexas(grafo):
     cfc = {}
     for i in range(len(At)):
         if At[i] is None:
-            if(i+1) not in cfc:
-                cfc[i+1] = [i+1]
+            if (i + 1) not in cfc:
+                cfc[i + 1] = [i + 1]
         else:
             indice = i
             while At[indice] is not None:
                 indice = At[indice] - 1
-            if (indice+1) not in cfc:
+            if (indice + 1) not in cfc:
                 cfc[indice + 1] = [indice + 1]
-            cfc[indice + 1].append(i+1)
+            cfc[indice + 1].append(i + 1)
 
     for c in cfc:
         cfc[c].sort()
         print(", ".join(map(str, cfc[c])))
     return At
 
+
+# --------------------- Q2 -----------------------------
 '''
 Entrada: grafo dirigido e não ponderado G=(V, A) e ordena linearmente todos os vértices
 tal que se existe um arco (u, v) ∈ A então u aparece antes de v na ordenação.
 '''
+
+
 def ordenacao_topologica(grafo):
     qtdVertices = grafo.qtdVertices()
-    foiVisitado = [False] * (qtdVertices+1)
+    foiVisitado = [False] * (qtdVertices + 1)
     ordem = []
-    #tempo = 0
+    # tempo = 0
 
     for each in grafo.getVertices():
         if (foiVisitado[each] == False):
@@ -206,103 +211,107 @@ def ordenacao_topologica(grafo):
             print(' > ', end='')
     print()
 
+
 def dfs_visit_ot(grafo, each, foiVisitado, ordem):
     foiVisitado[each] = True
-    #tempo = tempp + 1
+    # tempo = tempp + 1
 
     for i in grafo.vizinhos(each):
-         if (foiVisitado[each] == False):
-             dfs_visit_ot(grafo, each, foiVisitado, ordem)
+        if (foiVisitado[each] == False):
+            dfs_visit_ot(grafo, each, foiVisitado, ordem)
 
     ordem.append(each)
-    
-#--------------------- Q3 -----------------------------
+
+
+# --------------------- Q3 -----------------------------
 
 def prepare_sort(arestas, low, high):
-	pivot = arestas[high][2]
-	item = low - 1
-	
-	for i in range(low,high):
-		if arestas[i][2] <= pivot:
-			item = item + 1
-			(arestas[item], arestas[i]) = (arestas[i], arestas[item])
-			
-	(arestas[item+1], arestas[high]) = (arestas[high], arestas[item+1])
-	return item+1
-	
+    pivot = arestas[high][2]
+    item = low - 1
+
+    for i in range(low, high):
+        if arestas[i][2] <= pivot:
+            item = item + 1
+            (arestas[item], arestas[i]) = (arestas[i], arestas[item])
+
+    (arestas[item + 1], arestas[high]) = (arestas[high], arestas[item + 1])
+    return item + 1
+
+
 def quick_sort(arestas, low, high):
-	if low < high:
-		pivot = prepare_sort(arestas, low, high)
-		quick_sort(arestas, low, pivot-1)
-		quick_sort(arestas, pivot+1, high)
+    if low < high:
+        pivot = prepare_sort(arestas, low, high)
+        quick_sort(arestas, low, pivot - 1)
+        quick_sort(arestas, pivot + 1, high)
+
 
 def array_compare_different(a, b):
-	counter = 0
-	for i in a:
-		for j in b:
-			if i == j:
-				counter += counter
-				break;
-	if counter < len(a):
-		return True
-	else:
-		return False			
-		
-def _subarvore(vertice, subarvores):
-	for arvore_i in range(len(subarvores)):
-		for u in range(len(subarvores[arvore_i])):
-			if subarvores[arvore_i][u] == vertice:
-				return arvore_i
-	return 0
-    
-def kruskal(grafo):
-	arestas = []												# elemento de 'arestas[]' é uma tupla (u, v, peso)
-	matriz = grafo.matriz()										#
-	infinite = grafo.infinito									#
-	for i in range(grafo.qtdVertices()):						# constrói arranjo de arestas
-		for j in range(grafo.qtdVertices()):					#
-			if matriz[i][j] < infinite:							#
-				arestas.append((i, j, matriz[i][j]))			#
-	quick_sort(arestas, 0, len(arestas)-1)						# ordena as arestas
-	
-	arvoreGM = []
-	subarvores = [[] for _ in range(grafo.qtdVertices())]			# elemento de 'subarvores' é uma lista de vertices
-	x = []
-	
-	for i in range(len(subarvores)):
-		subarvores[i].append(i)
-	
-	somatoria = 0
-	for aresta in arestas:
-		xtemp = []
-		if array_compare_different(subarvores[aresta[0]], subarvores[aresta[1]]):
-			arvoreGM.append(aresta)
-			somatoria = somatoria + aresta[2]
-			xtemp = subarvores[aresta[0]]
-			xtemp.extend(subarvores[aresta[1]])
-			x = xtemp
-			for y in x:
-				subarvores[y] = x
+    counter = 0
+    for i in a:
+        for j in b:
+            if i == j:
+                counter += counter
+                break;
+    if counter < len(a):
+        return True
+    else:
+        return False
 
-	print_kruskal(arvoreGM, somatoria)
-			
+
+def _subarvore(vertice, subarvores):
+    for arvore_i in range(len(subarvores)):
+        for u in range(len(subarvores[arvore_i])):
+            if subarvores[arvore_i][u] == vertice:
+                return arvore_i
+    return 0
+
+
+def kruskal(grafo):
+    arestas = []  # elemento de 'arestas[]' é uma tupla (u, v, peso)
+    matriz = grafo.matriz()  #
+    infinite = grafo.infinito  #
+    for i in range(grafo.qtdVertices()):  # constrói arranjo de arestas
+        for j in range(grafo.qtdVertices()):  #
+            if matriz[i][j] < infinite:  #
+                arestas.append((i, j, matriz[i][j]))  #
+    quick_sort(arestas, 0, len(arestas) - 1)  # ordena as arestas
+
+    arvoreGM = []
+    subarvores = [[] for _ in range(grafo.qtdVertices())]  # elemento de 'subarvores' é uma lista de vertices
+    x = []
+
+    for i in range(len(subarvores)):
+        subarvores[i].append(i)
+
+    somatoria = 0
+    for aresta in arestas:
+        xtemp = []
+        if array_compare_different(subarvores[aresta[0]], subarvores[aresta[1]]):
+            arvoreGM.append(aresta)
+            somatoria = somatoria + aresta[2]
+            xtemp = subarvores[aresta[0]]
+            xtemp.extend(subarvores[aresta[1]])
+            x = xtemp
+            for y in x:
+                subarvores[y] = x
+
+    print_kruskal(arvoreGM, somatoria)
+
+
 def print_kruskal(arvoreGM, somatoria):
-	print(somatoria)
-	for aresta in arvoreGM:
-		print(aresta[0],aresta[1],sep='-',end=", ")
-	print("\b\b", end=" ")
-	print()
-		
+    print(somatoria)
+    for aresta in arvoreGM:
+        print(aresta[0], aresta[1], sep='-', end=", ")
+    print("\b\b", end=" ")
+    print()
+
 
 def a2():
-    # exe1
-    # grafo = Grafo("./tests/mesa_dfs.net")
-    # print("Componentes fortemente conexas")
-    # componentes_fortemente_conexas(grafo)
+    grafo = Grafo("./tests/caminho_minimo/fln_pequena.net")
 
-	grafo = Grafo("./tests/caminho_minimo/fln_pequena.net")
-	kruskal(grafo)
-    #exe2
-    #grafo = Grafo("./tests/dirigidos/manha.net")
-    #ordenacao_topologica(grafo)
-#-------------------------------------------------------------------------------
+    print("\n1 - Componentes fortemente conexas")
+    componentes_fortemente_conexas(grafo)
+    print("\n2 - Ordenação topologica")
+    ordenacao_topologica(grafo)
+    print("\n3 - Kruskal")
+    kruskal(grafo)
