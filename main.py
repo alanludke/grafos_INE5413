b1647c5b54fdb0f3291df877e95d7135ea2ff26c
rafaelbcastilhos <rafaelbcastilhos@gmail.com>
2021-09-16 20:56:22 -0300

    atualizando main method

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-import sys
 from AtividadeA1.a1 import a1
 from AtividadeA2.a2 import a2
+from AtividadeA3.a3 import a3
+
 
 def main():
-    #a1()
-    a2()
-    #a3
+    # a1()
+    # a2()
+    a3()
+
 
 if __name__ == '__main__':
     main()
